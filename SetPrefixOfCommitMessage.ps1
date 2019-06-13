ca930ef4a0d7bd0c490eb007f06613a82dc73d46
Kristian Jakubik <KristianJ@kentico.com>
2019-06-13 16:13:12 +0200

    Add test implementation

---
+++ SetPrefixOfCommitMessage.ps1
@@ -0,0 +1,45 @@
+param([string]$file)
+
+function GetCurrentCommitMessage {
+    param([string] $file)
+
+    return Get-Content $file
+}
+
+function GetBranchPrefix {
+    # $branchName = git rev-parse --abbrev-ref HEAD
+    $branchName = "feature/KCL-2196_repository_for_role_config_data"
+    $matches = $branchName | Select-String -Pattern '(\w+-\d+)'
+    $prefix = $matches.Matches[0].Groups[1].Value
+
+    return $prefix
+}
+
+function CheckIfContainsPrefix {
+    param([string] $message, [string] $prefix)
+    
+    return $message.StartsWith($prefix)
+}
+
+function SetPrefix{
+    param([string] $message, [string] $prefix)
+    
+    if ($(CheckIfContainsPrefix $message $prefix)) {
+        return $message
+    }
+    else {
+        return "$prefix - $message"
+    }
+}
+
+function SetNewCommitMessage {
+    param([string] $message, [string] $file)
+
+    Set-Content $file $message     
+}
+
+$message = GetCurrentCommitMessage $file
+$prefix = GetBranchPrefix
+$messageWithPrefix = SetPrefix $message $prefix
+SetNewCommitMessage $messageWithPrefix $file
+
